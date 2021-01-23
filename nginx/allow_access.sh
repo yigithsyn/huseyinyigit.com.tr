d857de2026a6f0ae6881742e2cad8c589920943c
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2021-01-23 21:40:31 +0300

    add ssl certificate

--- nginx/allow_access.sh
+++ nginx/allow_access.sh
@@ -3,4 +3,7 @@ gpasswd -a www-data root
 chmod g+x /root
 chmod g+x /root/sites
 chmod g+x /root/sites/huseyinyigit.com.tr
+chmod g+x /root/html
+chmod g+x /root/html/themes
+chmod g+x /root/html/themes/startbootstrap-resume
 nginx -s reload
